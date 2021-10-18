2bada796e3a38aea0cb6adf0212e3b3c15d05086
Jan Behrens <dev@jpbehrens.de>
2021-10-18 18:15:31 +0200

    update pdo and added readme

--- pdo.php
+++ pdo.php
@@ -1,7 +1,6 @@
 <?php
 
 require_once __DIR__.'/dbconfig.php';
-
 // Verbinden mit Datenbank 
 try {
     $pdo = new PDO('mysql:host='.$db["host"].';dbname='.$db["name"].';charset=utf8', $db["user"], $db["password"]);
@@ -10,20 +9,28 @@ try {
     echo 'Etwas ist schief gelaufen' . $e;
 }
 
-// Daten aus Datenbank Abrufen 
+// Daten aus Datenbank abrufen alle User
 function getUsers(){
     global $pdo;
-    return $pdo->query('SELECT firstname, lastname FROM users');
+    return $pdo->query('SELECT id, firstname, lastname FROM users');
 }
+
+// Einen User aus Datenbank abrufen
+
+function getUser($uid){
+    global $pdo;
+    return $pdo->query("SELECT id, firstname, lastname, bio FROM users WHERE id='$uid'");
+}
+
 
 function createUser(){
     global $pdo;
     $pdo->query("INSERT INTO `users` (`firstname`,`lastname`,`email`,`password`) VALUES ('John','Doe','john@doe.de','test123')");
 }
 
-function deleteUser(){
+function deleteUser($firstname){
     global $pdo;
-    $pdo->query("DELETE  FROM `users` WHERE firstname = 'John'");
+    $pdo->query("DELETE  FROM `users` WHERE firstname = '$firstname'");
 }
 
 function updateUser(){
