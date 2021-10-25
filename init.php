714db1d807837e232803d89e644baf6f17f52ce1
Jan Behrens <dev@jpbehrens.de>
2021-10-25 18:45:30 +0200

    prevent: crossSideScripting

--- init.php
+++ init.php
@@ -8,4 +8,12 @@ require_once __DIR__.'/autoload.php';
 
 define('MAINURL', 'http://localhost:8888/php-example-project');
 
+// Functions
+
+function html(string $string){
+    #prevents crossSideScripting
+    #use it as output of a database entry befor you print it out
+    return htmlentities($string, ENT_QUOTES, 'UFT-8');
+}
+
 $container = new App\App\Container();
